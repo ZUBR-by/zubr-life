2446643be68f68ee4d4827b5e7957923c2c75424
Alherd <algerd.blr@gmail.com>
2021-05-28 11:31:40 +0300

    Added sentry

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 import {defineConfig} from 'vite'
 import vue            from '@vitejs/plugin-vue'
 import styleImport    from 'vite-plugin-style-import'
+import viteSentry     from 'vite-plugin-sentry'
 
+const sentryConfig = {
+    url       : 'https://my.ondemand.sentry.com',
+    authToken : process.env.VITE_SENTRY_DSN,
+    release   : '1.0',
+    deploy    : {
+        env: 'production'
+    },
+    sourceMaps: {
+        include  : ['./dist/assets'],
+        ignore   : ['node_modules'],
+        urlPrefix: '~/assets'
+    }
+}
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
+        viteSentry(sentryConfig),
         styleImport({
             libs: [{
                 libraryName     : 'element-plus',
