032cbade22c143d7897ba23fcdae8f10fef848d0
Alherd <algerd.blr@gmail.com>
2021-06-04 20:01:15 +0300

    Various fixes

--- api/src/Person/GetPersonAction.php
+++ api/src/Person/GetPersonAction.php
@@ -5,29 +5,28 @@ namespace App\Person;
 use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 
 class GetPersonAction extends AbstractController
 {
-    public function __invoke(int $id, Request $request, Connection $connection) : JsonResponse
+    public function __invoke(int $id, Connection $connection) : JsonResponse
     {
         $data = $connection->fetchOne(<<<SQL
-SELECT JSON_OBJECT('data', JSON_OBJECT(
-            'id', p.id,
-            'full_name', full_name,
-            'description', description,
-            'photo_url', photo_url,
-            'org', JSON_OBJECT('id', o.id, 'name', o.name)
+    SELECT JSON_OBJECT('data', JSON_OBJECT(
+                'id', p.id,
+                'full_name', full_name,
+                'description', description,
+                'photo_url', photo_url,
+                'org', JSON_OBJECT('id', o.id, 'name', o.name)
+               )
            )
-       )
- FROM person as p
- JOIN persons_organizations po on p.id = po.person_id
- JOIN organization o on po.organization_id = o.id
-WHERE p.id = ?
+      FROM person as p
+ LEFT JOIN persons_organizations po on p.id = po.person_id
+ LEFT JOIN organization o on po.organization_id = o.id
+     WHERE p.id = ?
 SQL
             ,
             [$id]
         );
-        return JsonResponse::fromJsonString($data ?: '{"data":{}}}');
+        return JsonResponse::fromJsonString($data ?: '{"data":{}}');
     }
 }
