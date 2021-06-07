18c0b31006d95a2c1d3027ad602abb1a96b7a57c
Alherd <algerd.blr@gmail.com>
2021-06-07 13:04:05 +0300

    Страница персоны

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,9 +1,29 @@
 <template>
     <div class="section zbr-promo">
-        <div class="column is-centered">
-            {{ $route.params.id }}
-            {{ person }}
-            <button @click="click"> click</button>
+        <div class="columns">
+            <div style="padding-right: 40px">
+                <div class="person-photo">
+                    <img :src="person.photo_url
+                    ? person.photo_url
+                    : 'https://zubr.in/assets/images/user.svg'">
+                </div>
+            </div>
+
+            <div class="column is-four-fifths">
+                <h3 class="is-size-3">{{ person.full_name }}</h3>
+                <p v-if="person.org && person.description">
+                    {{ person.description }} в
+                    <router-link :to="{name: 'organization', params: {id: person.org.id}}">
+                        {{ person.org.name }}
+                    </router-link>
+                </p>
+                <p v-else-if="person.org">
+                    <router-link :to="{name: 'organization', params: {id: person.org.id}}">
+                        {{ person.org.name }}
+                    </router-link>
+                </p>
+                <p v-else>{{ person.description }}</p>
+            </div>
         </div>
     </div>
 </template>
@@ -22,9 +42,6 @@ export default {
         }
     },
     methods: {
-        click() {
-            throw 'error';
-        },
         fetchPerson() {
             fetch(import.meta.env.VITE_TELEGRAM_API_URL + '/person/' + this.$route.params.id)
                 .then(r => r.json())
@@ -38,6 +55,16 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.person-photo {
+    width: 150px;
+    height: 150px;
+    border-radius: 150px;
+    overflow: hidden;
 
+    img {
+        width: 150px;
+        border-radius: 150px;
+    }
+}
 </style>
