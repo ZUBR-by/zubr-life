ce5c883985416d90c729184563666f273b24f05a
Alherd <algerd.blr@gmail.com>
2021-06-08 17:09:00 +0300

    Страница персоны

--- api/src/Person/GetPersonAction.php
+++ api/src/Person/GetPersonAction.php
@@ -11,18 +11,30 @@ class GetPersonAction extends AbstractController
     public function __invoke(int $id, Connection $connection) : JsonResponse
     {
         $data = $connection->fetchOne(<<<SQL
-    SELECT JSON_OBJECT('data', JSON_OBJECT(
-                'id', p.id,
-                'full_name', full_name,
-                'description', description,
-                'photo_url', photo_url,
-                'org', JSON_OBJECT('id', o.id, 'name', o.name)
-               )
-           )
-      FROM person as p
- LEFT JOIN persons_organizations po on p.id = po.person_id
- LEFT JOIN organization o on po.organization_id = o.id
-     WHERE p.id = ?
+SELECT JSON_OBJECT(
+  'data', JSON_OBJECT(
+    'id', p.id,
+    'full_name', full_name,
+    'description', description,
+    'photo_url', photo_url,
+    'organizations_count', cast(COUNT(DISTINCT o.id) as integer),
+    'organizations', JSON_ARRAYAGG(DISTINCT JSON_OBJECT('id', o.id, 'name', o.name)),
+    'comments_count', cast(COUNT(DISTINCT c.id) as integer),
+    'comments', JSON_ARRAYAGG(
+      DISTINCT JSON_OBJECT(
+        'text', text,
+        'created_at', created_at,
+        'attachments', c.attachments,
+        'params', c.params
+          )
+        )
+      )
+    )
+FROM person as p
+     LEFT JOIN persons_organizations po on p.id = po.person_id
+     LEFT JOIN organization o on po.organization_id = o.id
+     LEFT JOIN comment c on p.id = c.person_id AND c.hidden_at IS NULL
+WHERE p.id = ?
 SQL
             ,
             [$id]
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,36 +1,87 @@
 <template>
     <div class="section zbr-promo">
-        <div class="columns">
-            <div style="padding-right: 40px">
-                <div class="person-photo">
-                    <img :src="person.photo_url
-                    ? person.photo_url
-                    : 'https://zubr.in/assets/images/user.svg'">
-                </div>
-            </div>
+        <div class="columns is-centered">
+            <div class="column is-two-thirds">
+                <div class="panel">
+                    <nav class="breadcrumb pt-5 pl-3 is-medium" aria-label="breadcrumbs">
+                        <ul>
+                            <li>
+                                <router-link :to="{name: 'people'}">
+                                    <button class="button is-primary is-inverted">
+                                        <span class="icon is-small">
+                                          <i class="fas fa-arrow-left"></i>
+                                        </span>
+                                        <span>Люди</span>
+                                    </button>
+                                </router-link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <hr>
+                    <div class="columns pl-3">
+                        <div class="column pl-5">
+                            <div class="person-photo">
+                                <img :src="person.photo_url ? person.photo_url : 'https://zubr.in/assets/images/user.svg'">
+                            </div>
+                        </div>
+                        <div class="column is-four-fifths">
+                            <h3 class="is-size-3">{{ person.full_name }}</h3>
+                        </div>
+                    </div>
+                    <div class="pl-5 pt-3 pb-4 pr-5" style="min-height: 300px;">
+                        <el-tabs v-model="activeName">
+                            <el-tab-pane label="Организации" name="orgs" style="overflow-x: auto">
+                                <table class="table is-fullwidth is-striped">
+                                    <tbody>
+                                    <tr v-for="org of person.organizations"
+                                        :key="org.id"
+                                        v-if="person.organizations_count > 0">
+                                        <td>
+                                            {{ person.description }} в
+                                            <router-link :to="{name: 'organization', params: {id: org.id}}">
+                                                {{ org.name }}
+                                            </router-link>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </el-tab-pane>
+                            <el-tab-pane :label="'Комментарии' + '(' + person.comments_count +')'" name="comments">
+                                <div class="card"
+                                     v-for="comment of person.comments"
+                                     v-if="person.comments_count > 0">
+                                    <div class="card-content">
+                                        <div class="content">
+                                            {{ comment.text }}
+                                        </div>
+                                    </div>
+                                    <footer class="card-footer">
+                                        <a href="#" class="card-footer-item">Author</a>
+                                        <a href="#" class="card-footer-item">Дата</a>
+                                    </footer>
+                                </div>
 
-            <div class="column is-four-fifths">
-                <h3 class="is-size-3">{{ person.full_name }}</h3>
-                <p v-if="person.org && person.description">
-                    {{ person.description }} в
-                    <router-link :to="{name: 'organization', params: {id: person.org.id}}">
-                        {{ person.org.name }}
-                    </router-link>
-                </p>
-                <p v-else-if="person.org">
-                    <router-link :to="{name: 'organization', params: {id: person.org.id}}">
-                        {{ person.org.name }}
-                    </router-link>
-                </p>
-                <p v-else>{{ person.description }}</p>
+                            </el-tab-pane>
+                        </el-tabs>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
+
+
 </template>
 
 <script>
 
+import {ElTabPane, ElTabs} from "element-plus";
+
 export default {
+    components: {
+        ElTabPane,
+        ElTabs
+    },
     created() {
         this.fetchPerson();
     },
@@ -39,6 +90,7 @@ export default {
         return {
             person: {},
             error : null,
+            activeName  : 'orgs'
         }
     },
     methods: {
