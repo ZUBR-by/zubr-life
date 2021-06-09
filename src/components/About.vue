c37af2f2f6269d9f6e36ec19bd662b6c5168ade7
Alherd <algerd.blr@gmail.com>
2021-06-09 13:12:11 +0300

    Добавлены ссылки на чаты

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,35 @@
 <template>
     <div class="section zbr-promo">
-        <div class="column is-centered">
-            Some html
+        <div class="columns is-centered">
+            <div class="column">
+                <h3>Ссылки</h3>
+                <ul>
+                    <li>
+                        <a href="https://t.me/loshitsa_united">Общий чат Лошицы в Телеграмме</a>
+                    </li>
+                    <li>
+                        <a href="https://t.me/volnayaLoshicaChat">Чат Вольной Лошицы</a>
+                    </li>
+                    <li>
+                        <a href="https://t.me/Loshitsa2">Лошица 2 и Соседи</a>
+                    </li>
+                    <li>
+                        <a href="https://t.me/loshica1">Лошица-1</a>
+                    </li>
+                    <li>
+                        <a href="https://t.me/goshkevicha22">Гошкевича 22</a>
+                    </li>
+                    <li>
+                        <a href="https://t.me/loshitza3">Лошица 3</a>
+                    </li>
+                    <li>
+                        <a href="https://t.me/loshitsa46">Лошица 46</a>
+                    </li>
+                    <li>
+                        <a href="https://vk.com/loshitsa1">Паблик Лошицы в ВК</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
