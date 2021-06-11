7ba054c71b023a4f45d6f40aa909e0358a3a6394
Alherd <algerd.blr@gmail.com>
2021-06-11 11:50:56 +0300

    Правки верстки

--- src/pages/People.vue
+++ src/pages/People.vue
@@ -1,50 +1,51 @@
 <template>
     <div class="section zbr-promo">
-        <div class="column is-centered">
-            <div class="content is-medium">
-                <h3 class="pl-5 pb-5 has-text-weight-bold">
+        <div class="columns is-centered is-fullwidth">
+            <div class="column">
+                <h3 class="content has-text-weight-bold is-medium pl-3">
                     Люди
                 </h3>
-            </div>
-            <table class="table is-fullwidth">
-                <thead>
-                <tr>
-                    <td>Фото</td>
-                    <th>ФИО</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="person of people">
-                    <td style="vertical-align: middle">
-                        <div class="grid-image">
-                            <img :src="person.photo_url
+                <table class="table is-striped">
+                    <thead>
+                    <tr>
+                        <td></td>
+                        <th>ФИО</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="person of people">
+                        <td style="vertical-align: middle">
+                            <div class="grid-image">
+                                <img :src="person.photo_url
                                         ? person.photo_url
                                         : 'https://zubr.in/assets/images/user.svg'">
-                        </div>
-                    </td>
-                    <td style="vertical-align: middle">
-                        <router-link :to="{name: 'person', params: {id: person.id}}">
-                            {{ person.full_name }}
-                        </router-link>
-                    </td>
-                    <td style="vertical-align: middle">
-                        <template v-if="person.description">
-                            {{ person.description }} в
-                            <router-link :to="{name: 'organization', params: {id: person.org.id}}">
-                                {{ person.org.name }}
+                            </div>
+                        </td>
+                        <td style="vertical-align: middle">
+                            <router-link :to="{name: 'person', params: {id: person.id}}">
+                                {{ person.full_name }}
                             </router-link>
-                        </template>
-                        <template v-else>
-                            <router-link :to="{name: 'organization', params: {id: person.org.id}}">
-                                {{ person.org.name }}
-                            </router-link>
-                        </template>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+                        </td>
+                        <td style="vertical-align: middle">
+                            <template v-if="person.description">
+                                {{ person.description }} в
+                                <router-link :to="{name: 'organization', params: {id: person.org.id}}">
+                                    {{ person.org.name }}
+                                </router-link>
+                            </template>
+                            <template v-else>
+                                <router-link :to="{name: 'organization', params: {id: person.org.id}}">
+                                    {{ person.org.name }}
+                                </router-link>
+                            </template>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
+
     </div>
 </template>
 
