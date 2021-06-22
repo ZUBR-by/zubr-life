03e7e2ccd5a0e1a3e1ce14dd049fc2f78b12dfac
Alherd <algerd.blr@gmail.com>
2021-06-22 12:54:54 +0300

    Правки верстки

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,37 +1,41 @@
 <template>
     <div class="section zbr-promo">
-        <div class="columns is-centered">
-            <div class="column">
-                <h3>Ссылки</h3>
-                <ul>
-                    <li>
-                        <a href="https://t.me/loshitsa_united">Общий чат Лошицы в Телеграмме</a>
-                    </li>
-                    <li>
-                        <a href="https://t.me/volnayaLoshicaChat">Чат Вольной Лошицы</a>
-                    </li>
-                    <li>
-                        <a href="https://t.me/Loshitsa2">Лошица 2 и Соседи</a>
-                    </li>
-                    <li>
-                        <a href="https://t.me/loshica1">Лошица-1</a>
-                    </li>
-                    <li>
-                        <a href="https://t.me/goshkevicha22">Гошкевича 22</a>
-                    </li>
-                    <li>
-                        <a href="https://t.me/loshitza3">Лошица 3</a>
-                    </li>
-                    <li>
-                        <a href="https://t.me/loshitsa46">Лошица 46</a>
-                    </li>
-                    <li>
-                        <a href="https://vk.com/loshitsa1">Паблик Лошицы в ВК</a>
-                    </li>
-                    <li>
-                        <a href="https://t.me/volnayaLoshica/1284">История микрарайона</a>
-                    </li>
-                </ul>
+        <div class="panel">
+            <div class="columns is-centered">
+                <div class="column">
+                    <article class="pl-4">
+                        <h3>Ссылки</h3>
+                        <ul>
+                            <li>
+                                <a href="https://t.me/loshitsa_united">Общий чат Лошицы в Телеграмме</a>
+                            </li>
+                            <li>
+                                <a href="https://t.me/volnayaLoshicaChat">Чат Вольной Лошицы</a>
+                            </li>
+                            <li>
+                                <a href="https://t.me/Loshitsa2">Лошица 2 и Соседи</a>
+                            </li>
+                            <li>
+                                <a href="https://t.me/loshica1">Лошица-1</a>
+                            </li>
+                            <li>
+                                <a href="https://t.me/goshkevicha22">Гошкевича 22</a>
+                            </li>
+                            <li>
+                                <a href="https://t.me/loshitza3">Лошица 3</a>
+                            </li>
+                            <li>
+                                <a href="https://t.me/loshitsa46">Лошица 46</a>
+                            </li>
+                            <li>
+                                <a href="https://vk.com/loshitsa1">Паблик Лошицы в ВК</a>
+                            </li>
+                            <li>
+                                <a href="https://t.me/volnayaLoshica/1284">История микрорайона</a>
+                            </li>
+                        </ul>
+                    </article>
+                </div>
             </div>
         </div>
     </div>
