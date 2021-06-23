64019404dd2baddfe6a90deec8d3ad751092b8f7
Alherd <algerd.blr@gmail.com>
2021-06-23 12:42:02 +0300

    Видео галерея!

--- src/components/gallery.vue
+++ src/components/gallery.vue
@@ -1,12 +1,26 @@
 <template>
     <lightgallery :settings="{ speed: 500, plugins: plugins }">
-        <template v-for="item of collection">
+        <template v-for="(item, index) of collection">
             <a v-if="item.type === 'image'" :data-lg-size="item.size"
-               class="gallery-item"
+               class="gallery-item pl-1"
                :data-src="item.value">
                 <img :src="item.value">
             </a>
+            <a  v-if="item.type === 'video'"
+                data-lg-size="1280-720"
+                :data-video='JSON.stringify(
+                    {
+                        "source": [{"src": item.value, "type":"video/mp4"}],
+                        "attributes": {"preload": false, "controls": true}
+                    })'
+            >
+                <img src="/imgs/video-thumb.jpg"
+                     class="pl-1"
+                     style="width: 340px;height: 180px;"
+                     :alt="'Видео ' + (index + 1)"/>
+            </a>
         </template>
+
     </lightgallery>
 </template>
 
@@ -14,10 +28,12 @@
 import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-thumbnail.css';
 import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-video.css';
 
 import Lightgallery from 'lightgallery/vue';
 import lgThumbnail  from 'lightgallery/plugins/thumbnail';
 import lgZoom       from 'lightgallery/plugins/zoom';
+import lgVideo from 'lightgallery/plugins/video';
 
 
 export default {
@@ -28,7 +44,7 @@ export default {
         collection: Array
     },
     data      : () => ({
-        plugins: [lgThumbnail, lgZoom],
+        plugins: [lgThumbnail, lgZoom, lgVideo],
     }),
 }
 </script>
