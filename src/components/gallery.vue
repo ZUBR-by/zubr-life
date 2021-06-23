e781b6c3328d423f0457cd6ceab0f8db47067a51
Alherd <algerd.blr@gmail.com>
2021-06-23 12:12:14 +0300

    Обкатываю новую галерею для медиа файлов

--- src/components/gallery.vue
+++ src/components/gallery.vue
@@ -1,45 +1,47 @@
 <template>
-    <el-carousel trigger="click" :arrow="'always'" :autoplay="false" v-if="collection.length > 0" style="max-height: 800px">
-        <el-carousel-item v-for="item of collection" :key="item.value">
-            <div class="columns is-centered">
-                <el-image :src="item.value"
-                          v-if="item.type === 'image'"
-                          class="pl-6 pr-6"
-                          :preview-src-list="[item.value]"
-                          :append-to-body="true"
-                          style="width: 600px;height: 600px;margin:auto"
-                          :fit="'scale-down'"></el-image>
-                <div v-else-if="item.type === 'video'">
-                    <video controls style="height: auto;">
-                        <source :src="item.value">
-                    </video>
-                </div>
-                <template v-else>
-                    {{item}}
-                </template>
-            </div>
-        </el-carousel-item>
-    </el-carousel>
+    <lightgallery :settings="{ speed: 500, plugins: plugins }">
+        <template v-for="item of collection">
+            <a v-if="item.type === 'image'" :data-lg-size="item.size"
+               class="gallery-item"
+               :data-src="item.value">
+                <img :src="item.value">
+            </a>
+        </template>
+    </lightgallery>
 </template>
 
 <script>
-import {ElCarousel, ElCarouselItem, ElImage} from "element-plus";
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-thumbnail.css';
+import 'lightgallery/css/lg-zoom.css';
+
+import Lightgallery from 'lightgallery/vue';
+import lgThumbnail  from 'lightgallery/plugins/thumbnail';
+import lgZoom       from 'lightgallery/plugins/zoom';
+
 
 export default {
     components: {
-        ElCarousel, ElCarouselItem, ElImage
+        Lightgallery,
     },
     props     : {
         collection: Array
     },
-
+    data      : () => ({
+        plugins: [lgThumbnail, lgZoom],
+    }),
 }
 </script>
 
 <style>
-.el-carousel__container {
-    min-height: 600px;
-    max-height: 1000px;
-    position: relative;
+.img-responsive {
+    width: 300px;
+    height: 100px;
 }
+
+.gallery-item img {
+    width: 30%;
+    height: auto;
+}
+
 </style>
