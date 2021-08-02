860794ba55d518d900cc457f14f714b6772f6db0
Alherd <algerd.blr@gmail.com>
2021-08-02 21:44:54 +0300

    Fixed bntu flag

--- src/pages/Navbar.vue
+++ src/pages/Navbar.vue
@@ -64,7 +64,7 @@ export default {
                     '1.jpg'
                 ],
                 'bntu' : [
-                    '0.bntu'
+                    '0.gif'
                 ]
             }
 
