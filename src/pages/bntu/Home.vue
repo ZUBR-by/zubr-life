d25237e92e4f63482d275760c586ba689b5d87eb
dizollate <1zanin135@gmail.com>
2021-12-01 21:01:11 +0100

    feat: add first block on main page layout

--- src/pages/bntu/Home.vue
+++ src/pages/bntu/Home.vue
@@ -1,86 +1,97 @@
 <template>
-  <div class="section zbr-promo" style="margin-bottom: 200px">
-    <div class="columns">
-      <div class="column is-three-fifths">
-        <div class="content is-medium">
-          <h1 class="pl-5 pb-5 has-text-weight-bold">
-            Экран студенческого сообщества - {{ name }}
+  <div class="bntu-home">
+    <section class="bntu-sections">
+      <div class="home-main">
+        <div class="main-content">
+          <h1 class="main-title">
+            Мы — независимое объединение студентов и работников Белорусского
+            национального технического университета, которые решили запустить
+            альтернативный сайт БНТУ
           </h1>
-          <ul class="pr-6 pb-3">
-            <li>
-              <p>
-                Держи руку на пульсе того, что происходит в сообществе и следи за <a href="/feed">
-                Лентой новостей</a>
-              </p>
-            </li>
-            <li>
-              <p>
-                Оценивай и комментируй работу должностных лиц на странице
-                <a href="/rating">рейтинга</a>
-              </p>
-            </li>
-          </ul>
+          <span class="main-text">
+            Мы отличаемся тем, что публикуем честные новости, размещаем полезную
+            информацию для студентов в разделе “Репозиторий” и неравнодушием к
+            проблемам университета.
+          </span>
+          <div class="buttons">
+            <div class="bntu-button bntu-button-main">Кнопка</div>
+            <div class="bntu-button bntu-button-secondary">Кнопка</div>
+          </div>
         </div>
       </div>
       <div class="column">
         <div class="content">
           <h1>Последние новости</h1>
           <div class="announcements" v-if="data && data.activities">
-            <router-link :to="{name: 'activity', params: {id: item.id}}"
-                         class="box"
-                         v-for="item of data.activities">
-              <div class="is-size-7 has-text-grey">{{ formatDate(item.created_at) }}</div>
-              <div class="tag"
-                   :class="{'is-primary' : item.category === 'AD', 'is-danger': item.category !== 'AD'}">
-                {{ item.type === 'event' ? 'Событие' : 'Объявление' }}
+            <router-link
+              :to="{ name: 'activity', params: { id: item.id } }"
+              class="box"
+              v-for="item of data.activities"
+              v-bind:key="item"
+            >
+              <div class="is-size-7 has-text-grey">
+                {{ formatDate(item.created_at) }}
               </div>
-              <p><strong>{{ item.title }}</strong></p>
+              <div
+                class="tag"
+                :class="{
+                  'is-primary': item.category === 'AD',
+                  'is-danger': item.category !== 'AD',
+                }"
+              >
+                {{ item.type === "event" ? "Событие" : "Объявление" }}
+              </div>
+              <p>
+                <strong>{{ item.title }}</strong>
+              </p>
             </router-link>
           </div>
           <div class="pt-5">
-            <router-link class="button is-fullwidth" :to="'/feed'">Еще</router-link>
+            <router-link class="button is-fullwidth" :to="'/feed'"
+              >Еще</router-link
+            >
           </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
 <script>
-import {defineComponent, ref} from "vue";
-import {useQuery} from "@urql/vue";
-import {formatDate} from "../../date";
+import { defineComponent, ref } from "vue";
+import { useQuery } from "@urql/vue";
+import { formatDate } from "../../date";
 
-const community = typeof slug !== 'undefined' ? slug : 'unknown';
+const community = typeof slug !== "undefined" ? slug : "unknown";
 
 const communityMap = {
-  'loshitsa': {
-    name: 'Лошица',
-    zoom: 14.70,
-    center: [27.580935, 53.844329]
+  loshitsa: {
+    name: "Лошица",
+    zoom: 14.7,
+    center: [27.580935, 53.844329],
   },
-  'vitebsk': {
-    'name': 'Витебск',
-    zoom: 12.70,
-    center: [30.2043, 55.1918]
+  vitebsk: {
+    name: "Витебск",
+    zoom: 12.7,
+    center: [30.2043, 55.1918],
   },
-  'bntu': {
-    'name': 'БНТУ',
-    zoom: 15.70,
-    center: [27.593069412931786, 53.92111407553088]
+  bntu: {
+    name: "БНТУ",
+    zoom: 15.7,
+    center: [27.593069412931786, 53.92111407553088],
   },
-  'unknown': {
-    'name': 'Терра Инкогнито',
-    zoom: 12.70,
-    center: [0, 0]
-  }
-}
+  unknown: {
+    name: "Терра Инкогнито",
+    zoom: 12.7,
+    center: [0, 0],
+  },
+};
 
 export default defineComponent({
   setup() {
     const result = useQuery({
-          // language=GraphQL
-          query: `
+      // language=GraphQL
+      query: `
 query ($community: String!) {
     activities: community_activity(
         where: {
@@ -98,11 +109,10 @@ query ($community: String!) {
     }
 }
       `,
-          variables: {
-            community: slug
-          }
-        }
-    )
+      variables: {
+        community: slug,
+      },
+    });
     let feature = ref(null);
     return {
       fetching: result.fetching,
@@ -112,61 +122,84 @@ query ($community: String!) {
       formatDate,
       name: communityMap[community].name,
       feature,
-    }
+    };
   },
-})
+});
 </script>
 <style>
-@media (max-height: 820px) {
-  .map-home {
-    padding-right: 1.7em;
-  }
+.bntu-home {
+  padding: 0 !important;
+  margin-top: 110px;
+  max-width: 100vw;
+  font-family: Fira Sans sans-serif !important;
+}
+.bntu-sections {
+  display: flex;
+  flex-direction: column;
 }
 
-.ol-popup {
-  position: absolute;
-  background-color: white;
-  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
-  padding: 15px;
+.home-main {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(100vh - 110px);
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("/imgs/bntu/landing_back.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.main-content {
+  display: flex;
+  flex-direction: column;
+  max-width: 960px;
+}
+
+.main-title {
+  max-width: 768px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 34px;
+  line-height: 41px;
+  color: #fff;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  margin-bottom: 18px;
+}
+
+.main-text {
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 26px;
+  color: #ffffff;
+  margin-bottom: 40px;
+}
+
+.buttons {
+  display: flex;
+  align-items: center;
+}
+
+.bntu-button {
+  padding: 17px 55px;
+  font-weight: 900;
+  font-size: 18px;
+  line-height: 22px;
+  color: #ffffff;
   border-radius: 10px;
-  border: 1px solid #cccccc;
-  bottom: 12px;
-  left: -50px;
-  min-width: 280px;
+  text-align: center;
+  cursor: pointer;
 }
 
-.ol-popup:after, .ol-popup:before {
-  top: 100%;
-  border: solid transparent;
-  content: " ";
-  height: 0;
-  width: 0;
-  position: absolute;
-  pointer-events: none;
+.bntu-button-main {
+  background: #d32121;
+  border: none;
+  margin-right: 36px;
 }
 
-.ol-popup:after {
-  border-top-color: white;
-  border-width: 10px;
-  left: 48px;
-  margin-left: -10px;
-}
-
-.ol-popup:before {
-  border-top-color: #cccccc;
-  border-width: 11px;
-  left: 48px;
-  margin-left: -11px;
-}
-
-.ol-popup-closer {
-  text-decoration: none;
-  position: absolute;
-  top: 2px;
-  right: 8px;
-}
-
-.ol-popup-closer:after {
-  content: "✖";
+.bntu-button-secondary {
+  background: transparent;
+  border: 2px solid #ffffff;
 }
 </style>
