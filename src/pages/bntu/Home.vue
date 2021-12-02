f65723847fb04cd790a2e43550518353b23c0b24
dizollate <1zanin135@gmail.com>
2021-12-02 18:02:21 +0100

    feat: layout 3 blocks

--- src/pages/bntu/Home.vue
+++ src/pages/bntu/Home.vue
@@ -2,55 +2,103 @@
   <div class="bntu-home">
     <section class="bntu-sections">
       <div class="home-main">
-        <div class="main-content">
-          <h1 class="main-title">
-            Мы — независимое объединение студентов и работников Белорусского
-            национального технического университета, которые решили запустить
-            альтернативный сайт БНТУ
-          </h1>
-          <span class="main-text">
-            Мы отличаемся тем, что публикуем честные новости, размещаем полезную
-            информацию для студентов в разделе “Репозиторий” и неравнодушием к
-            проблемам университета.
-          </span>
-          <div class="buttons">
-            <div class="bntu-button bntu-button-main">Кнопка</div>
-            <div class="bntu-button bntu-button-secondary">Кнопка</div>
+        <div class="main-content-wrapper">
+          <div class="main-content">
+            <h1 class="main-title">
+              Мы — независимое объединение студентов и работников Белорусского
+              национального технического университета, которые решили запустить
+              альтернативный сайт БНТУ
+            </h1>
+            <span class="main-text">
+              Мы отличаемся тем, что публикуем честные новости, размещаем
+              полезную информацию для студентов в разделе “Репозиторий” и
+              неравнодушием к проблемам университета.
+            </span>
+            <div class="buttons">
+              <div class="bntu-button bntu-button-main">Репозиторий</div>
+              <div class="bntu-button bntu-button-secondary">Люди</div>
+            </div>
           </div>
         </div>
       </div>
-      <div class="column">
-        <div class="content">
-          <h1>Последние новости</h1>
-          <div class="announcements" v-if="data && data.activities">
-            <router-link
-              :to="{ name: 'activity', params: { id: item.id } }"
-              class="box"
+      <div class="home-boxes">
+        <div class="home-boxes-wrapper">
+          <div class="home-box home-box-repository">
+            <div class="home-box-background home-box-repository"></div>
+            <div class="home-box-main-text">Репозиторий</div>
+            <div class="home-box-main-secondary">
+              Здесь собрана полезная литература, курсы, ссылки на информационные
+              ресурсы, а также готовые контрольные, лабораторные, курсовые
+              проекты для студентов всех факультетов.<br /><br />
+              Периодически мы будем обновлять репозиторий, для этого нам
+              необходима ваша помощь: если вам есть чем поделиться состудентами
+              своей специальности — присылайте анонимно файл в наш телеграм-бот.
+            </div>
+          </div>
+          <div class="home-box">
+            <div class="home-box-main-text">Люди</div>
+            <div class="home-box-background home-box-people"></div>
+            <div class="home-box-main-secondary">
+              Здесь собрана информация о преподавателях и других работниках
+              университета. Тут можно узнать: что требует тот или иной
+              преподаватель от студентов, ведет ли учет посещаемости и как
+              относится к студентам.<br /><br />
+              Под описанием каждый может оставить отзыв о работнике, поставить
+              ему оценку, что сформирует рейтинг. Мы должны знать героев в лицо!
+              Если вы хотите добавить своего преподавателя в раздел “Люди” —
+              напишите в наш телеграм-бот.
+            </div>
+          </div>
+          <div class="home-box">
+            <div class="home-box-main-text">Проблемы</div>
+            <div class="home-box-background home-box-problems"></div>
+            <div class="home-box-main-secondary">
+              Здесь мы рассказываем о проблемах в университете от лица студентов
+              и работников БНТУ, а также путях их решения.<br /><br />
+              Каждый может написать комментарий под проблемой, чтобы повысить ее
+              значимость. Анонимно рассказать о проблеме можно в нашем
+              телеграм-боте.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bntu-news">
+        <div class="bntu-news-line"></div>
+        <div class="bntu-news-wrapper">
+          <h2 class="bntu-news-title">Новости</h2>
+          <div class="bntu-news-contents" v-if="data && data.activities">
+            <div
+              class="bntu-news-content"
               v-for="item of data.activities"
               v-bind:key="item"
             >
-              <div class="is-size-7 has-text-grey">
-                {{ formatDate(item.created_at) }}
+              <span class="bntu-news-content-title">
+                {{ item.title }}
+              </span>
+              <!-- <span class="bntu-news-content-description">
+                {{ item.description.replace(item.title, "") }}
+              </span> -->
+              <div class="bntu-news-bottom">
+                <div class="is-size-7 has-text-grey">
+                  {{ formatDate(item.created_at) }}
+                </div>
+                <router-link
+                  :to="{ name: 'activity', params: { id: item.id } }"
+                  class="bntu-news-bottom-more"
+                  >Читать подробнее</router-link
+                >
               </div>
-              <div
-                class="tag"
-                :class="{
-                  'is-primary': item.category === 'AD',
-                  'is-danger': item.category !== 'AD',
-                }"
-              >
-                {{ item.type === "event" ? "Событие" : "Объявление" }}
-              </div>
-              <p>
-                <strong>{{ item.title }}</strong>
-              </p>
-            </router-link>
+            </div>
           </div>
-          <div class="pt-5">
-            <router-link class="button is-fullwidth" :to="'/feed'"
-              >Еще</router-link
-            >
-          </div>
+          <router-link class="bntu-button bntu-button-white" :to="'/feed'"
+            >Читать больше</router-link
+          >
+        </div>
+      </div>
+      <div class="bntu-contacts">
+        <div class="bntu-contacts-wrapper">
+          <h3 class="bntu-contacts-title">Контакты</h3>
+          <div class="bntu-contacts-content"></div>
         </div>
       </div>
     </section>
@@ -106,6 +154,7 @@ query ($community: String!) {
         description
         extra
         created_at
+        attachments
     }
 }
       `,
@@ -128,10 +177,9 @@ query ($community: String!) {
 </script>
 <style>
 .bntu-home {
-  padding: 0 !important;
-  margin-top: 110px;
-  max-width: 100vw;
+  position: relative;
   font-family: Fira Sans sans-serif !important;
+  margin-top: 110px;
 }
 .bntu-sections {
   display: flex;
@@ -140,21 +188,33 @@ query ($community: String!) {
 
 .home-main {
   width: 100%;
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
   min-height: calc(100vh - 110px);
-  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url("/imgs/bntu/landing_back.png");
+  height: calc(100vh - 110px);
+  background: url("/imgs/bntu/landing_back.png");
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
 }
 
+.main-content-wrapper {
+  width: 100%;
+  height: 430px;
+  background: rgba(0, 0, 0, 0.65);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 .main-content {
   display: flex;
   flex-direction: column;
-  max-width: 960px;
+  align-items: flex-start;
+  width: 100%;
+  max-width: 1480px;
+  padding: 0 20px;
 }
 
 .main-title {
@@ -169,11 +229,13 @@ query ($community: String!) {
 }
 
 .main-text {
+  max-width: 960px;
   font-weight: 500;
   font-size: 22px;
   line-height: 26px;
   color: #ffffff;
   margin-bottom: 40px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 
 .buttons {
@@ -194,12 +256,255 @@ query ($community: String!) {
 
 .bntu-button-main {
   background: #d32121;
-  border: none;
+  border: 2px solid #d32121;
   margin-right: 36px;
+  transition: all 0.4s ease;
+}
+
+.bntu-button-white {
+  background: #fff;
+  color: #000;
+  border: 1px solid #000000;
+  transition: all 0.4s ease;
+  text-decoration: none;
+}
+
+.bntu-button-main:hover {
+  background: #f50909;
+  border: 2px solid #ffffff;
 }
 
 .bntu-button-secondary {
   background: transparent;
   border: 2px solid #ffffff;
+  transition: all 0.6s ease;
+}
+
+.bntu-button-secondary:hover {
+  background: #ffffff;
+  color: #000;
+}
+
+.home-boxes {
+  margin-top: 140px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.home-boxes-wrapper {
+  width: 100%;
+  height: 380px;
+  max-width: 1480px;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.home-box {
+  height: 100%;
+  width: calc(100% / 3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  cursor: pointer;
+  transition: all 0.4s ease;
+  z-index: 2;
+}
+
+.home-box-main-text {
+  opacity: 1;
+  text-align: center;
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 30px;
+  line-height: 36px;
+  color: #ffffff;
+  text-transform: uppercase;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  transition: all 0.4s ease;
+}
+
+.home-box:hover .home-box-main-text {
+  opacity: 0;
+}
+
+.home-box:hover {
+  filter: brightness(77%);
+}
+
+.home-box-background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  filter: brightness(100%);
+  z-index: -1;
+  transition: all 0.4s ease;
+}
+
+.home-box-main-secondary {
+  position: absolute;
+  width: 90%;
+  text-align: center;
+  align-items: center;
+  opacity: 0;
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  color: #ffffff;
+  transition: all 0.4s ease;
+  z-index: 10;
+}
+
+.home-box:hover .home-box-background {
+  filter: brightness(65%);
+}
+
+.home-box:hover .home-box-main-secondary {
+  opacity: 1;
+}
+
+.home-box-repository {
+  background: url("/imgs/bntu/repository.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.home-box-people {
+  background: url("/imgs/bntu/people.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.home-box-problems {
+  background: url("/imgs/bntu/problems.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.bntu-news {
+  padding: 115px 0;
+  width: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.bntu-news-line {
+  position: absolute;
+  width: 220px;
+  height: 100%;
+  background: #d32121;
+  top: 50%; /* position the top  edge of the element at the middle of the parent */
+  left: 50%; /* position the left edge of the element at the middle of the parent */
+
+  transform: translate(-50%, -50%);
+  z-index: -2;
+}
+
+.bntu-news-wrapper {
+  width: 100%;
+  max-width: 1480px;
+  padding: 0 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.bntu-news-contents {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+}
+
+.bntu-news-title {
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 34px;
+  line-height: 41px;
+  text-transform: uppercase;
+  color: #ffffff;
+  margin-bottom: 80px;
+}
+
+.bntu-news-content-description {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 145%;
+  color: #000000;
+  text-overflow: ellipsis;
+}
+
+.bntu-news-contents {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 60px;
+}
+
+.bntu-news-content {
+  text-decoration: none;
+  width: 30%;
+  height: 450px;
+  background: #ffffff;
+  filter: drop-shadow(0px 0px 20px rgba(0, 0, 0, 0.3));
+  border-radius: 15px;
+  padding: 25px 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  transition: all 0.3s ease;
+}
+
+.bntu-news-content:hover {
+  transform: scale(1.03);
+}
+
+.bntu-news-content-title {
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 26px;
+  text-transform: uppercase;
+  color: #000000;
+  margin-bottom: 20px;
+}
+.bntu-news-bottom {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.bntu-news-bottom-more {
+  font-family: Fira Sans;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+  text-decoration-line: underline;
+  color: #d32121;
+  transition: all 0.4s ease;
+}
+
+.bntu-news-bottom-more:hover {
+  text-decoration-line: none;
 }
 </style>
--- src/pages/bntu/Navbar.vue
+++ src/pages/bntu/Navbar.vue
@@ -1,100 +1,141 @@
 <template>
-    <div class="box is-radiusless zbr-hdr">
-        <div class="columns is-align-items-center">
-            <div class="column is-two-fifth pl-1">
-                <div>
-                    <a href="/" style="float: left">
-                        <img class="zbr-h-logo" :src="flag">
-                    </a>
-                    <a id="widget" style="float: left;padding-top:20px;padding-left: 5px"></a>
-                </div>
+  <div class="bntu-navbar">
+    <div class="bntu-navbar-wrapper">
+      <a href="/" class="navbar-img-link">
+        <img class="navbar-img" src="/imgs/bntu/logo.jpg" alt="bntu-logo" />
+      </a>
 
-                <div class="has-text-weight-medium pr-6">
-                    <input id="brgrbtn" class="is-hidden-desktop" type="checkbox" v-model="hidden">
-                    <label for="brgrbtn" class="burger-button is-hidden-desktop mr-5">
-                        <div class="burger-button-line"></div>
-                        <div class="burger-button-line"></div>
-                        <div class="burger-button-line"></div>
-                    </label>
-                    <div class="box is-radiusless pt-6 brgr-nav is-hidden-desktop">
-                        <aside class="menu pt-6">
-                            <p class="menu-label">
-                                Разделы сайта
-                            </p>
-                            <ul class="menu-list">
-                                <li v-for="(route, i) of routes" :key="route.name">
-                                    <a href="https://drive.google.com/drive/folders/1MReZiH6w3LkclE3cK_RhDtI9W7Gx3SG4"
-                                       v-if="i === (routes.length - 2)">
-                                      Репозиторий
-                                    </a>
-                                    <router-link :to="{name: route.name}" @click="hidden = false">
-                                        {{ route.label }}
-                                    </router-link>
-                                </li>
-                            </ul>
-                        </aside>
-                    </div>
-                </div>
-            </div>
-            <div class="column is-three-fifths has-text-right has-text-weight-medium is-hidden-mobile">
-                <template v-for="(route, i) of routes" :key="route.name">
-                    <a class="ml-5"
-                       target="_blank"
-                       href="https://drive.google.com/drive/folders/1J9NNrlqAKeDWA2a0bLqsvDk7nBmTEX-D"
-                       v-if="i === (routes.length - 2)">
-                      Репозиторий
-                    </a>
-                    <router-link :to="{name: route.name}" class="ml-5">
-                        {{ route.label }}
-                    </router-link>
-                </template>
-            </div>
-        </div>
+      <div class="navbar-bntu-links">
+        <template v-for="(route, i) of routes" :key="route.name">
+          <a
+            class="navbar-bntu-link"
+            target="_blank"
+            href="https://drive.google.com/drive/folders/1J9NNrlqAKeDWA2a0bLqsvDk7nBmTEX-D"
+            v-if="i === routes.length - 2"
+          >
+            Репозиторий
+          </a>
+          <router-link :to="{ name: route.name }" class="navbar-bntu-link">
+            {{ route.label }}
+          </router-link>
+        </template>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import {routes} from "../../router";
+import { routes } from "../../router";
 
 export default {
-    data() {
-        return {
-            hidden: false,
-            routes: routes.filter(i => i.label)
-        }
-    },
-    computed: {
-        flag() {
-            const map = {
-                'loshitsa': [
-                    '0.png',
-                    '1.png',
-                    '2.png'
-                ],
-                'vitebsk' : [
-                    '1.jpg'
-                ],
-                'bntu' : [
-                    '0.jpg'
-                ]
-            }
+  data() {
+    return {
+      hidden: false,
+      routes: routes.filter((i) => i.label),
+    };
+  },
+  computed: {
+    flag() {
+      const map = {
+        loshitsa: ["0.png", "1.png", "2.png"],
+        vitebsk: ["1.jpg"],
+        bntu: ["0.jpg"],
+      };
 
-            return '/imgs/' + slug + '/' + map[slug][Math.floor(Math.random() * map[slug].length)];
-        }
+      return (
+        "/imgs/" +
+        slug +
+        "/" +
+        map[slug][Math.floor(Math.random() * map[slug].length)]
+      );
     },
-    mounted() {
-        let telegramScript = document.createElement('script')
-        telegramScript.setAttribute('src', 'https://telegram.org/js/telegram-widget.js?14')
-        telegramScript.setAttribute('async', '')
-        telegramScript.setAttribute('data-telegram-login', 'zubr_life_' + slug + '_bot')
-        telegramScript.setAttribute('data-userpic', 'false')
-        telegramScript.setAttribute('data-size', 'medium')
-        telegramScript.setAttribute('data-auth-url', import.meta.env.VITE_TELEGRAM_AUTH_URL)
-        // document.getElementById("widget").appendChild(telegramScript)
-    }
-}
+  },
+  mounted() {
+    let telegramScript = document.createElement("script");
+    telegramScript.setAttribute(
+      "src",
+      "https://telegram.org/js/telegram-widget.js?14"
+    );
+    telegramScript.setAttribute("async", "");
+    telegramScript.setAttribute(
+      "data-telegram-login",
+      "zubr_life_" + slug + "_bot"
+    );
+    telegramScript.setAttribute("data-userpic", "false");
+    telegramScript.setAttribute("data-size", "medium");
+    telegramScript.setAttribute(
+      "data-auth-url",
+      import.meta.env.VITE_TELEGRAM_AUTH_URL
+    );
+    // document.getElementById("widget").appendChild(telegramScript)
+  },
+};
 </script>
 
-<style scoped>
+<style>
+.bntu-navbar {
+  width: 100%;
+  height: 110px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  background: #fff;
+  z-index: 20;
+}
 
+.bntu-navbar-wrapper {
+  width: 100%;
+  height: 100%;
+  max-width: 1480px;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.navbar-img-link {
+  all: initial;
+  display: block;
+  width: 140px;
+  height: 90px;
+  cursor: pointer;
+  filter: drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.1));
+  transition: all 0.4s ease;
+}
+.navbar-img-link:hover {
+  transform: scale(1.1);
+}
+
+.navbar-bntu-links {
+  display: flex;
+  align-items: center;
+  height: 100%;
+}
+
+.navbar-bntu-link {
+  all: initial;
+  font-family: Fira Sans;
+
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 26px;
+  cursor: pointer;
+  color: #000000;
+  transition: all 0.4s ease;
+}
+.navbar-bntu-link:not(:last-child) {
+  margin-right: 35px;
+}
+.navbar-bntu-link:hover {
+  color: #d32121;
+}
+
+.navbar-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
 </style>
