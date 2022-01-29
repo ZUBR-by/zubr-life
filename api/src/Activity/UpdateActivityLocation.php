7b7a9efabe43b202cc89767a090466e7d53874d2
Alherd <algerd.blr@gmail.com>
2022-01-29 20:41:40 +0200

    Update only if provided

--- api/src/Activity/UpdateActivityLocation.php
+++ api/src/Activity/UpdateActivityLocation.php
@@ -13,11 +13,11 @@ use function Psl\Json\decode;
 
 class UpdateActivityLocation extends AbstractController implements BotAuthentication
 {
-    public function __invoke(Request $request, GraphQLClient $graphQLClient, LoggerInterface $logger) : JsonResponse
+    public function __invoke(Request $request, GraphQLClient $graphQLClient, LoggerInterface $logger): JsonResponse
     {
         /** @psalm-suppress PossiblyInvalidArgument */
         $payload = decode($request->getContent());
-        if (! isset($payload['uniqueId'])) {
+        if (!isset($payload['uniqueId'])) {
             return new JsonResponse(['error' => 'provide unique id']);
         }
         $query = /** @lang GraphQL */
@@ -31,8 +31,15 @@ mutation($point: geometry, $uid: String!, $json: jsonb) {
     }
 }
 GraphQL;
-        if (! isset($payload['lng'], $payload['lat'])) {
+        if (!isset($payload['lng'], $payload['lat'])) {
             return new JsonResponse(['error' => 'not found lng or lat']);
+        }
+        $json = [];
+        if ($payload['region'] ?? false) {
+            $json['region'] = $payload['region'];
+        }
+        if ($payload['area'] ?? false) {
+            $json['area'] = $payload['area'];
         }
         try {
             $graphQLClient->requestAuth(
@@ -43,10 +50,7 @@ GraphQL;
                         'type'        => 'Point',
                         'coordinates' => [$payload['lng'], $payload['lat']],
                     ],
-                    'json'  => [
-                        'region' => $payload['region'] ?? '',
-                        'area'   => $payload['area'] ?? '',
-                    ],
+                    'json'  => $json,
                 ]
             );
         } catch (Throwable $e) {
