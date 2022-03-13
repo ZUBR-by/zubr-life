5ba52ffe3a9cdd02c16769d2248c319eaf1c8c9d
Alherd <algerd.blr@gmail.com>
2022-03-13 16:53:21 +0200

    Formatting attachments

--- api/src/Activity/GetActivities.php
+++ api/src/Activity/GetActivities.php
@@ -12,16 +12,16 @@ use function Psl\Str\lowercase;
 
 class GetActivities extends AbstractController
 {
-    public function __invoke(Request $request, GraphQLClient $graphQLClient) : JsonResponse
+    public function __invoke(Request $request, GraphQLClient $graphQLClient): JsonResponse
     {
         $date  = [
-                'month'  => '-1 month',
-                'month3' => '-3 month',
-                'month6' => '-6 month',
-                'day'    => '-1 day',
-                'week'   => '-1 week',
-                'day3'   => '-3 days',
-            ][$request->get('time')] ?? '-1 month';
+                     'month'  => '-1 month',
+                     'month3' => '-3 month',
+                     'month6' => '-6 month',
+                     'day'    => '-1 day',
+                     'week'   => '-1 week',
+                     'day3'   => '-3 days',
+                 ][$request->get('time')] ?? '-1 month';
         $query = /** @lang GraphQL */
             <<<'GraphQL'
 query($timestamp: timestamp!)  {
@@ -46,6 +46,13 @@ query($timestamp: timestamp!)  {
         communities {
             community_id
         }
+        files {
+            attachment {
+                url
+                content_type
+                extra
+            }
+        }
     }
 }
 GraphQL;
@@ -54,14 +61,22 @@ GraphQL;
             ['timestamp' => (new DateTime($date))->setTime(0, 0)->format(DATE_ATOM)]
         );
 
+        $formatAttachmentType = fn(string $raw): string => str_contains($raw, '/') ? \Psl\Str\split($raw, '/')[0] : $raw;
         return new JsonResponse([
             'result'     => map(
                 $data['community_activity'],
-                fn (array $item) : array => [
-                    'id'         => $item['id'],
+                fn(array $item): array => [
+                    'id'          => $item['id'],
                     'direction'   => lowercase($item['category']),
-                    'type'        => $item['attachments'][0]['type'] ?? 'text',
-                    'attachments' => $item['attachments'],
+                    'type'        => $formatAttachmentType($item['files'][0]['attachment']['content_type'] ?? 'text'),
+                    'attachments' => map(
+                        $item['files'],
+                        fn(array $raw): array => [
+                            'url'   => $raw['attachment']['url'],
+                            'type'  => $formatAttachmentType($raw['attachment']['content_type']),
+                            'thumb' => $raw['extra']['thumb'] ?? ''
+                        ]
+                    ),
                     'geometry'    => $item['geometry'],
                     'description' => $item['description'],
                     'area'        => $item['extra']['area'] ?? '',
